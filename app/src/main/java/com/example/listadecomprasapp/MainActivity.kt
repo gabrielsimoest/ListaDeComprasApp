e5c6d2d4cec198a6e899fc6be3e3ba3dfdad63b2
rafaeldonascimento2 <139973470+rafaeldonascimento2@users.noreply.github.com>
2024-09-28 16:26:19 -0300

    Lista
    utilizei o activity_home, MainActivity e activity_list

--- app/src/main/java/com/example/listadecomprasapp/MainActivity.kt
+++ app/src/main/java/com/example/listadecomprasapp/MainActivity.kt
@@ -3,10 +3,50 @@ package com.example.listadecomprasapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.listadecomprasapp.databinding.ActivityMainBinding
+import com.example.listadecomprasapp.home.data.ListaDeCompras
+import com.example.listadecomprasapp.home.data.ListasAdapter
 import com.example.listadecomprasapp.login.ui.LoginActivity
 
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var listasAdapter: ListasAdapter
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Configurando o ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Verifica se o usuário está logado e redireciona para a tela de login se necessário
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+
+        // Configurando o RecyclerView para exibir as listas de compras
+        binding.listsRecyclerView.layoutManager = GridLayoutManager(this, 2)
+
+        val listasDeCompras = listOf(
+            ListaDeCompras("Semanal", R.drawable.ic_placeholder_image),
+            ListaDeCompras("Supermercado", R.drawable.ic_placeholder_image),
+            ListaDeCompras("Feira", R.drawable.ic_placeholder_image),
+            ListaDeCompras("Título", R.drawable.ic_placeholder_image),
+            ListaDeCompras("Title", R.drawable.ic_placeholder_image),
+            ListaDeCompras("Title", R.drawable.ic_placeholder_image)
+        )
+
+        listasAdapter = ListasAdapter(listasDeCompras)
+        binding.listsRecyclerView.adapter = listasAdapter
+
+        // Configura o botão flutuante para adicionar uma nova lista
+        binding.fab.setOnClickListener {
+            // Ação para adicionar uma nova lista
+        }
+    }
+}
+
+/* o que estava antes:
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
@@ -23,3 +63,4 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+ */
