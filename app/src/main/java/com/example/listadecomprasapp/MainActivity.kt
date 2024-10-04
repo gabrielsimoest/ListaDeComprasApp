66be53f475ae139e3daca6b9da7ad1684f50f993
Gabriel Simões <gabrielsimoest@gmail.com>
2024-10-04 10:45:44 -0300

    Revert "Arrumando o codigo"
    This reverts commit e40427faba5a354f69cc347b69898b7d44e63773.

--- app/src/main/java/com/example/listadecomprasapp/MainActivity.kt
+++ app/src/main/java/com/example/listadecomprasapp/MainActivity.kt
@@ -16,13 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Configurando o ViewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Verifica se o usuário está logado e redireciona para a tela de login se necessário
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
 
-        // RecyclerView para exibir as listas de compras
+        // Configurando o RecyclerView para exibir as listas de compras
         binding.listsRecyclerView.layoutManager = GridLayoutManager(this, 2)
 
         val listasDeCompras = listOf(
@@ -39,7 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         // Configura o botão flutuante para adicionar uma nova lista
         binding.fab.setOnClickListener {
-
+            // Ação para adicionar uma nova lista
         }
     }
 }
